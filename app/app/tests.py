d1d243d1eef5147a1832c0a04a33fea7b4ce1db1
NoisyAeon <johannessebastian.see@gmail.com>
2022-08-17 10:49:46 +0200

    Added untracked files

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        response = calc.add(5, 6)
+
+        self.assertEqual(response, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        response = calc.subtract(10, 15)
+
+        self.assertEqual(response, 5)
